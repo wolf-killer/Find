689217088235c7a2eba11535c2e78eeaffee0310
Freda Wong <98999744+wolf-killer@users.noreply.github.com>
2024-03-07 17:03:38 +0800

    Update Setting.js

--- js/Setting.js
+++ js/Setting.js
@@ -70,29 +70,6 @@ function selectGate(row, col) {
   }
 }
 
-function createSampleAirplane(direction) {
-  sampleAirport = [];
-  for (var row = 0; row < sampleAirportLength; row++) {
-    var airportGate = [];
-    for (var col = 0; col < sampleAirportLength; col++) {
-      airportGate.push({ ...defaultCell });
-    }
-    sampleAirport.push(airportGate);
-  }
-
-  var headDirection = direction;
-  var xHead = direction == 1 ? airplane1PossibleArea[headDirection].xStart+1: airplane1PossibleArea[headDirection].xStart;
-  var yHead = direction == 2 ? airplane1PossibleArea[headDirection].yStart+1: airplane1PossibleArea[headDirection].yStart;
-	
-  var planeDemo = airplane1[headDirection];
-  for (var i = 0; i < planeDemo.length; i++) {
-    var xAdjust = xHead + planeDemo[i].x;
-    var yAdjust = yHead - planeDemo[i].y;
-    sampleAirport[xAdjust][yAdjust].cellDefinition = planeBody;
-  }
-  sampleAirport[xHead][yHead].cellDefinition = planeHead;
-}
-
 function showUpdateDialog() {
   var inputObject = [
     {
@@ -144,6 +121,29 @@ function updateDefaultSetting() {
   airportLength = Number($("#inputAirportLength").val());
   noOfPlaneHead = $("#inputNoOfPlaneHead").val();
   main();
+}
+
+function createSampleAirplane(direction) {
+  sampleAirport = [];
+  for (var row = 0; row < sampleAirportLength; row++) {
+    var airportGate = [];
+    for (var col = 0; col < sampleAirportLength; col++) {
+      airportGate.push({ ...defaultCell });
+    }
+    sampleAirport.push(airportGate);
+  }
+
+  var headDirection = direction;
+  var xHead = direction == 1 ? airplane1PossibleArea[headDirection].xStart+1: airplane1PossibleArea[headDirection].xStart;
+  var yHead = direction == 2 ? airplane1PossibleArea[headDirection].yStart+1: airplane1PossibleArea[headDirection].yStart;
+	
+  var planeDemo = airplane1[headDirection];
+  for (var i = 0; i < planeDemo.length; i++) {
+    var xAdjust = xHead + planeDemo[i].x;
+    var yAdjust = yHead - planeDemo[i].y;
+    sampleAirport[xAdjust][yAdjust].cellDefinition = planeBody;
+  }
+  sampleAirport[xHead][yHead].cellDefinition = planeHead;
 }
 
 function getSamplePlaneHtml(transparent, direction) {
@@ -204,6 +204,100 @@ function displaySampleAirplane(){
 function showSamplePlaneDialog(){
   var sampleAirplaneHtml = getSamplePlaneHtml(false, 0);
   SHOW_ALERT("M", "REMARK", "模板", sampleAirplaneHtml);
+}
+
+function createInstructionAirplane() {
+  instructionAirport = [];
+  for (var row = 0; row < instructionAirportLength; row++) {
+    var airportGate = [];
+    for (var col = 0; col < instructionAirportLength; col++) {
+      airportGate.push({ ...defaultCell });
+    }
+    instructionAirport.push(airportGate);
+  }
+	
+	var loopingNoOfPlaneHead = noOfInstructionAirplane;
+	for(var i=0; i< noOfInstructionAirplane; i++){
+		createInstructionAirplaneImplement(i);
+	}
+}
+
+function createInstructionAirplaneImplement(loopingNoOfPlaneHead) {
+	console.log("loopingNoOfPlaneHead", loopingNoOfPlaneHead);
+  var headDirection = instructionAirplaneLocation[loopingNoOfPlaneHead].direction;
+  var xHead = instructionAirplaneLocation[loopingNoOfPlaneHead].xStart;
+  var yHead = instructionAirplaneLocation[loopingNoOfPlaneHead].yStart;
+  
+	if (checkAirplaneValid(headDirection, xHead, yHead)) {
+		var planeId = loopingNoOfPlaneHead;
+    var planeDemo = airplane1[headDirection];
+    for (var i = 0; i < planeDemo.length; i++) {
+      var xAdjust = xHead + planeDemo[i].x;
+      var yAdjust = yHead - planeDemo[i].y;
+      instructionAirport[xAdjust][yAdjust].cellDefinition = planeBody;
+			instructionAirport[xAdjust][yAdjust].planeId = planeId;
+    }
+    instructionAirport[xHead][yHead].cellDefinition = planeHead;
+		instructionAirport[xHead][yHead].planeId = planeId;
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function getInstructionAirportHtml() {
+  createInstructionAirplane();
+	var setInstructionAirportWidth = setScreenWidth * 0.25;
+	var setInstructionAirportHeight = setScreenWidth * 0.25;
+	
+	var newInstructionAirport = $("<table></table>");
+	newInstructionAirport.addClass("sampleAirport w3-table");
+	newInstructionAirport.css("width", setInstructionAirportWidth + "px");
+	newInstructionAirport.css("height", setInstructionAirportHeight + "px");
+	newInstructionAirport.css("margin", "auto");
+	
+  for (var row = 0; row < instructionAirportLength; row++) {
+		var newInstructionAirportRow = $("<tr></tr>");
+    for (var col = 0; col < instructionAirportLength; col++) {
+			var newInstructionAirportCol = $("<td></td>");
+			newInstructionAirportCol.addClass("airportGate");
+      switch (instructionAirport[row][col].cellDefinition) {
+        case planeHead: {
+					newInstructionAirportCol.addClass("planeHead");
+          break;
+        }
+        case planeBody: {
+					newInstructionAirportCol.addClass("planeBody");
+          break;
+        }
+        default: {
+					newInstructionAirportCol.addClass("emptyCell");
+          break;
+        }
+      }
+			newInstructionAirportRow.append(newInstructionAirportCol);
+    }
+		newInstructionAirport.append(newInstructionAirportRow);
+  }
+  return newInstructionAirport;
+}
+
+
+function showInstructionDialog(){
+	var newInstructionTab = $("<div></div>");
+	newInstructionTab.css("display", "flex");
+	
+	var newInstructionText = $("<div></div>");
+	newInstructionText.css("flex", "50%");
+	newInstructionText.text("棋盤中隱藏多架飛機，玩家需通過推理找出全部紅色的飛機頭。");
+	
+	var newInstructionAirport = $("<div></div>");
+	newInstructionAirport.css("flex", "50%");
+	newInstructionAirport.append(getInstructionAirportHtml());
+	
+	newInstructionTab.append(newInstructionText);
+	newInstructionTab.append(newInstructionAirport);
+	SHOW_ALERT("L", "REMARK", "遊戲說明", newInstructionTab);
 }
 
  function updateGameResult(cellDefinition) {
