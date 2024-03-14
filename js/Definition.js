0bd2da7f3b104547c284c513acab2da0845662a2
Freda Wong <98999744+wolf-killer@users.noreply.github.com>
2024-03-14 15:38:32 +0800

    Update Definition.js

--- js/Definition.js
+++ js/Definition.js
@@ -20,14 +20,15 @@ var instructionAirport;
 var instructionAirportLength = 10;
 var noOfInstructionAirplane = 3;
 var instructionAirplaneLocation = [
-  { xStart: 0, yStart: 7, direction: 0, airplaneId: 1 },
-  { xStart: 4, yStart: 0, direction: 2, airplaneId: 1 },
-  { xStart: 9, yStart: 6, direction: 1, airplaneId: 1 }
+  { xStart: 0, yStart: 7, direction: 0, graphicId: 1 },
+  { xStart: 4, yStart: 0, direction: 2, graphicId: 0 },
+  { xStart: 9, yStart: 6, direction: 1, graphicId: 0 }
 ]
 
 var airport;
 var airportLength = 10;
 var noOfPlaneHead = 2;
+var selectedGraphic;
 
 var gameStepCount = 0;
 var gameRemainHead = noOfPlaneHead;
@@ -65,129 +66,130 @@ const screenInstruction = [
     }
   ]
 
-const airplane = [
+const airplane = 
 [
-  [
-    //up
-      { x: +0, y: +0 },
-      { x: +1, y: +2 },
-      { x: +1, y: +1 },
-      { x: +1, y: +0 },
-      { x: +1, y: -1 },
-      { x: +1, y: -2 },
-      { x: +2, y: +0 },
-      { x: +3, y: +1 },
-      { x: +3, y: +0 },
-      { x: +3, y: -1 },
-  ],
-  [
-    //down
-      { x: +0, y: +0 },
-      { x: -1, y: +2 },
-      { x: -1, y: +1 },
-      { x: -1, y: +0 },
-      { x: -1, y: -1 },
-      { x: -1, y: -2 },
-      { x: -2, y: +0 },
-      { x: -3, y: +1 },
-      { x: -3, y: +0 },
-      { x: -3, y: -1 },
-  ],
-  [
-    //left
-      { x: +0, y: +0 },
-      { x: +0, y: -1 },
-      { x: -1, y: -1 },
-      { x: -2, y: -1 },
-      { x: +1, y: -1 },
-      { x: +2, y: -1 },
-      { x: +0, y: -2 },
-      { x: +0, y: -3 },
-      { x: -1, y: -3 },
-      { x: +1, y: -3 },
-  ],
-  [
-    //right
-      { x: +0, y: +0 },
-      { x: +0, y: +1 },
-      { x: -1, y: +1 },
-      { x: -2, y: +1 },
-      { x: +1, y: +1 },
-      { x: +2, y: +1 },
-      { x: +0, y: +2 },
-      { x: +0, y: +3 },
-      { x: -1, y: +3 },
-      { x: +1, y: +3 },
-  ]
-],
-[
-  [
-    //up
-      { x: +0, y: +0 },
-      { x: +1, y: +1 },
-      { x: +1, y: +0 },
-      { x: +1, y: -1 },
-      { x: +2, y: +2 },
-      { x: +2, y: +0 },
-      { x: +2, y: -2 },
-      { x: +3, y: +0 },
-      { x: +4, y: +1 },
-      { x: +4, y: +0 },
-      { x: +4, y: -1 },
-  ],
-  [
-    //down
-      { x: +0, y: +0 },
-      { x: -1, y: +1 },
-      { x: -1, y: +0 },
-      { x: -1, y: -1 },
-      { x: -2, y: +2 },
-      { x: -2, y: +0 },
-      { x: -2, y: -2 },
-      { x: -3, y: +0 },
-      { x: -4, y: +1 },
-      { x: -4, y: +0 },
-      { x: -4, y: -1 },
-  ],
-  [
-    //left
-      { x: +0, y: +0 },
-      { x: +1, y: -1 },
-      { x: +0, y: -1 },
-      { x: -1, y: -1 },
-      { x: +2, y: -2 },
-      { x: +0, y: -2 },
-      { x: -2, y: -2 },
-      { x: +0, y: -3 },
-      { x: +1, y: -4 },
-      { x: +0, y: -4 },
-      { x: -1, y: -4 },
-  ],
-  [
-    //right
-      { x: +0, y: +0 },
-      { x: +1, y: +1 },
-      { x: +0, y: +1 },
-      { x: -1, y: +1 },
-      { x: +2, y: +2 },
-      { x: +0, y: +2 },
-      { x: -2, y: +2 },
-      { x: +0, y: +3 },
-      { x: +1, y: +4 },
-      { x: +0, y: +4 },
-      { x: -1, y: +4 },
-  ]
-]
+	[ // graphic 1
+		[
+			//up
+				{ x: +0, y: +0 },
+				{ x: +1, y: +2 },
+				{ x: +1, y: +1 },
+				{ x: +1, y: +0 },
+				{ x: +1, y: -1 },
+				{ x: +1, y: -2 },
+				{ x: +2, y: +0 },
+				{ x: +3, y: +1 },
+				{ x: +3, y: +0 },
+				{ x: +3, y: -1 },
+		],
+		[
+			//down
+				{ x: +0, y: +0 },
+				{ x: -1, y: +2 },
+				{ x: -1, y: +1 },
+				{ x: -1, y: +0 },
+				{ x: -1, y: -1 },
+				{ x: -1, y: -2 },
+				{ x: -2, y: +0 },
+				{ x: -3, y: +1 },
+				{ x: -3, y: +0 },
+				{ x: -3, y: -1 },
+		],
+		[
+			//left
+				{ x: +0, y: +0 },
+				{ x: +0, y: -1 },
+				{ x: -1, y: -1 },
+				{ x: -2, y: -1 },
+				{ x: +1, y: -1 },
+				{ x: +2, y: -1 },
+				{ x: +0, y: -2 },
+				{ x: +0, y: -3 },
+				{ x: -1, y: -3 },
+				{ x: +1, y: -3 },
+		],
+		[
+			//right
+				{ x: +0, y: +0 },
+				{ x: +0, y: +1 },
+				{ x: -1, y: +1 },
+				{ x: -2, y: +1 },
+				{ x: +1, y: +1 },
+				{ x: +2, y: +1 },
+				{ x: +0, y: +2 },
+				{ x: +0, y: +3 },
+				{ x: -1, y: +3 },
+				{ x: +1, y: +3 },
+		]
+	],
+	[ // graphic 2
+		[
+			//up
+				{ x: +0, y: +0 },
+				{ x: +1, y: +1 },
+				{ x: +1, y: +0 },
+				{ x: +1, y: -1 },
+				{ x: +2, y: +2 },
+				{ x: +2, y: +0 },
+				{ x: +2, y: -2 },
+				{ x: +3, y: +0 },
+				{ x: +4, y: +1 },
+				{ x: +4, y: +0 },
+				{ x: +4, y: -1 },
+		],
+		[
+			//down
+				{ x: +0, y: +0 },
+				{ x: -1, y: +1 },
+				{ x: -1, y: +0 },
+				{ x: -1, y: -1 },
+				{ x: -2, y: +2 },
+				{ x: -2, y: +0 },
+				{ x: -2, y: -2 },
+				{ x: -3, y: +0 },
+				{ x: -4, y: +1 },
+				{ x: -4, y: +0 },
+				{ x: -4, y: -1 },
+		],
+		[
+			//left
+				{ x: +0, y: +0 },
+				{ x: +1, y: -1 },
+				{ x: +0, y: -1 },
+				{ x: -1, y: -1 },
+				{ x: +2, y: -2 },
+				{ x: +0, y: -2 },
+				{ x: -2, y: -2 },
+				{ x: +0, y: -3 },
+				{ x: +1, y: -4 },
+				{ x: +0, y: -4 },
+				{ x: -1, y: -4 },
+		],
+		[
+			//right
+				{ x: +0, y: +0 },
+				{ x: +1, y: +1 },
+				{ x: +0, y: +1 },
+				{ x: -1, y: +1 },
+				{ x: +2, y: +2 },
+				{ x: +0, y: +2 },
+				{ x: -2, y: +2 },
+				{ x: +0, y: +3 },
+				{ x: +1, y: +4 },
+				{ x: +0, y: +4 },
+				{ x: -1, y: +4 },
+		]
+	]
 ];
 
 const airplanePossibleArea = [
-  [
+  [ // graphic 1
     { xStart: 0, xEnd: -4, yStart: 2, yEnd: -3 }, //up
     { xStart: 3, xEnd: -1, yStart: 2, yEnd: -3 }, //down
     { xStart: 2, xEnd: -3, yStart: 0, yEnd: -4 }, //left
     { xStart: 2, xEnd: -3, yStart: 3, yEnd: -1 } //right
   ],
-  [
+  [ // graphic 2
     { xStart: 0, xEnd: -5, yStart: 2, yEnd: -3 }, //up
     { xStart: 4, xEnd: -1, yStart: 2, yEnd: -3 }, //down
     { xStart: 2, xEnd: -3, yStart: 0, yEnd: -5 }, //left
